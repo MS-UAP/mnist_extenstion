30d9fa11b74f4b5e5e5232100cd48b93ccddab9d
chelxom <chelxom@outlook.com>
2018-10-26 15:35:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# 这是 MNIST 扩展实践
+
+# 目录结构
+* 相关代码 [extended_mnist_calculator/ 目录](./extended_mnist_calculator/)
+* 训练代码 [tensorflow_model/ 目录](./tensorflow_model/)
+
+# List of dependencies
+
+- [./tensorflow_model/convolutional.py](./tensorflow_model/convolutional.py)
+    - Origin: <https://github.com/tensorflow/models/blob/f81bb397efe57cf8bfb4a195c1b3064997f3e3c2/tutorials/image/mnist/convolutional.py>
---
+++ extended_mnist_calculator/MNIST.App/Program.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MNIST.App
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
